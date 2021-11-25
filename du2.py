594f8c6e61e7744678898c7847658261cee6a626
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2021-11-25 12:52:12 +0100

    vytvoření writeru a zápis do výstupních souborů

--- du2.py
+++ du2.py
@@ -1,11 +1,14 @@
 import csv
 
-with open('prutoky.csv', encoding="utf-8") as csvfile,\
-    open("prutoky.csv", encoding="utf-8") as csvoutfile:
-    reader = csv.reader(csvfile, delimiter =",")
-    writer = csv.writer(csvoutfile, delimiter =",")  
- 
-    citac_dni = 0 
+#otevření souborů 
+with open('prutoky.csv', encoding="utf-8") as csvfile, open("vystup_7dni.csv", "w", newline="", encoding="utf-8") as csvoutfile_7dni,\
+    open("vystup_rok.csv", "w", newline="", encoding="utf-8") as csvoutfile_rok:
+    reader = csv.reader(csvfile)
+    writer_7dni = csv.writer(csvoutfile_7dni)
+    writer_rok = csv.writer(csvoutfile_rok)  
+
+    #nastavení proměnných pro výpočet sedmidenních průtoků 
+    pocet_dni = 0 
     vysledek = 0
     prutok = 0
     zbyle_dny = 0
@@ -13,30 +16,38 @@ with open('prutoky.csv', encoding="utf-8") as csvfile,\
     #lines= len(list(reader))    #počet řádků 
     #print(lines)
 
+    #cyklus pro procházení souboru
     for row in reader:
         zbyle_dny += 1
-        if citac_dni % 7 == 0:
+        #zápis prvního dne v týdnu
+        if pocet_dni % 7 == 0:
             prvniDenVtydnu = row 
             print(prvniDenVtydnu)
         try:
             prutok += float(row[5])
         except ValueError:
             print("Something's wrong.")
-        if  citac_dni % 7 == 6:
+        #výpočet průtoku v průběhu týdne 
+        if  pocet_dni % 7 == 6:
             vysledek = prutok / 7
             prutok = 0
             zbyle_dny = 0 
-            print(vysledek)
+            prvniDenVtydnu[5] = f" {vysledek:.4f}"
+            writer_7dni.writerow(prvniDenVtydnu)
+            print(vysledek) #SMAZAT POTOM
             vysledek = 0
-        citac_dni += 1  
-
-    if citac_dni % 7 != 6:
+        pocet_dni += 1  
+    #výpočet průtoku za zbylé dny
+    if pocet_dni % 7 != 6:
         vysledek = prutok / zbyle_dny
+        prvniDenVtydnu[5] = f" {vysledek:.4f}"
         print(vysledek)
 
+    #nastavení proměnných pro výpočet ročních průtoků
     rok = 0
     dny_vroce= 0
     citac_roku = 0
+
     for row in reader:
     #pro roky
         if citac_roku == rok:
