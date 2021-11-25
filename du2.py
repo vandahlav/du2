18b41e65ec84d1ef2841628ff7ccc3e51bd4e558
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2021-11-25 12:23:38 +0100

    sedmidenní průtoky

--- du2.py
+++ du2.py
@@ -4,38 +4,45 @@ with open('prutoky.csv', encoding="utf-8") as csvfile,\
     open("prutoky.csv", encoding="utf-8") as csvoutfile:
     reader = csv.reader(csvfile, delimiter =",")
     writer = csv.writer(csvoutfile, delimiter =",")  
-
-    citac_prutoku = 0 
+ 
     citac_dni = 0 
     vysledek = 0
     prutok = 0
+    zbyle_dny = 0
 
-    lines= len(list(reader))    #počet řádků 
-    print(lines)
+    #lines= len(list(reader))    #počet řádků 
+    #print(lines)
 
     for row in reader:
+        zbyle_dny += 1
         if citac_dni % 7 == 0:
-            citac_prutoku = 0
-            citac_dni = 0
-        
-
-
-
-"""
-    for row in reader:
+            prvniDenVtydnu = row 
+            print(prvniDenVtydnu)
         try:
-            prutok = float(row[5])
+            prutok += float(row[5])
         except ValueError:
+            print("Something's wrong.")
+        if  citac_dni % 7 == 6:
+            vysledek = prutok / 7
             prutok = 0
-        finally:
-            citac_dni =+ 1
-        vysledek = prutok + vysledek
-        if citac_dni == 7:
-            prutok = float(row[5])
-            vysledek = prutok + vysledek
-            citac_prutoku = 0
-            citac_dni = 0
-        if citac_dni % 7 != 0:      
-    print(vysledek)
-"""
-        
+            zbyle_dny = 0 
+            print(vysledek)
+            vysledek = 0
+        citac_dni += 1  
+
+    if citac_dni % 7 != 6:
+        vysledek = prutok / zbyle_dny
+        print(vysledek)
+
+    rok = 0
+    dny_vroce= 0
+    citac_roku = 0
+    for row in reader:
+    #pro roky
+        if citac_roku == rok:
+            prvniDenVroce = row
+        if citac_roku != rok:
+            vysledek_2 = rok / dny_vroce
+            prvniDenVroce = row
+            citac_roku = 0
+        dny_vroce = dny_vroce + 1
